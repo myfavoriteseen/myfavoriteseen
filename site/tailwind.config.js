79234d4b76f297522109ae6cdfa7f04db9a79a6b
myfavoriteseen <myfavoriteseen@gmail.com>
2025-09-24 10:39:13 -0400

    Delete site directory

--- site/tailwind.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
-  theme: { extend: { colors: { neon: '#7afcff', accent: '#ff4dd2', bg: '#050014' } } },
-  plugins: [],
-}
